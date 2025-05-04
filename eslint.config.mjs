769be155519f09585a87349dbce1bbfb48b0e6d5
ernestognw <ernestognw@gmail.com>
2025-05-03 20:17:30 -0600

    Up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from '@eslint/compat';
 import { defineConfig } from 'eslint/config';
+import { fileURLToPath } from 'node:url';
+
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
+  includeIgnoreFile(gitignorePath),
   { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
